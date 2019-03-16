6f1a92223706442a9ed68611d5bc462f2ca412b2
Sawyer McLane <samclane@gmail.com>
2019-03-15 18:44:09 -0600

    first commit

---
+++ game.h
@@ -0,0 +1,34 @@
+/*
+ * game.h
+ *
+ *  Created on: Mar 15, 2019
+ *      Author: SawyerPC
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+#define N 3 // rows & columns
+#define SCREEN_WIDTH 640.0
+#define SCREEN_HEIGHT 480.0
+#define CELL_WIDTH (SCREEN_WIDTH / N)
+#define CELL_HEIGHT (SCREEN_HEIGHT / N)
+
+#define EMPTY 0
+#define PLAYER_X 1
+#define PLAYER_O 2
+
+#define RUNNING_STATE 0
+#define PLAYER_X_WON_STATE 1
+#define PLAYER_O_WON_STATE 2
+#define TIE_STATE 3
+#define QUIT_STATE 4
+
+typedef struct {
+	int board[N * N];
+	int player;
+	int state;
+} game_t;
+
+
+#endif /* GAME_H_ */
